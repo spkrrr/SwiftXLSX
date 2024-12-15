afd4f203fe03bf3f639e412437faa02b8a753cf5
spkrrr <admin@spkrrr.com>
2024-12-15 15:55:15 +0800

    fix: dependencies

--- Package.swift
+++ Package.swift
@@ -1,28 +1,52 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.8
 import PackageDescription
 
 let package = Package(
-    name: "SwiftXLSX",
+    name: shouldUseOldPlatformName() ? "SwiftXLSXForOldPlatforms" : "SwiftXLSX",
     platforms: [
         .iOS(.v10),
         .macOS(.v11),
     ],
     products: [
         .library(
-            name: "SwiftXLSX",
-            targets: ["SwiftXLSX"]),
+            name: shouldUseOldPlatformName() ? "SwiftXLSXForOldPlatforms" : "SwiftXLSX",
+            targets: ["SwiftXLSX"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", "2.3.0"..<"2.5.0")
+        // 动态决定依赖版本
+        .package(
+            url: "https://github.com/ZipArchive/ZipArchive.git",
+            shouldUseOldPlatformName() ? "2.3.0"..<"2.5.0" : .upToNextMajor(from: "2.5.0")
+        )
     ],
     targets: [
         .target(
             name: "SwiftXLSX",
-            dependencies: ["ZipArchive"]),
+            dependencies: ["ZipArchive"]
+        ),
         .testTarget(
             name: "SwiftXLSXTests",
-            dependencies: ["SwiftXLSX"]),
+            dependencies: ["SwiftXLSX"]
+        ),
     ]
 )
+
+/// 判断是否应使用旧平台配置
+func shouldUseOldPlatformName() -> Bool {
+    #if os(iOS)
+    if #available(iOS 13, *) {
+        return false
+    }
+    #endif
+
+    #if os(macOS)
+    if #available(macOS 11, *) {
+        return false
+    }
+    #endif
+
+    return true
+}
